97c9fdf239b6eede461d4ba7822f7bc4d2cedf3b
keiSanoBtc <kei.sano@bigtreetc.com>
2022-05-20 12:41:57 +0900

    ユーザーコントローラーを変種

--- itemApp1-1/src/main/java/com/example/demo/controller/UserController.java
+++ itemApp1-1/src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public String helloWorld(Model model) {
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String userPlanBookList(@ModelAttribute UserSearchRequest userSearchRequest, Model model) {
+    	List<Book> userPlanBookList = userService.userPlanBookList(userSearchRequest);
+        model.addAttribute("planBookList", userPlanBookList);
         return "user";
     }
 }
