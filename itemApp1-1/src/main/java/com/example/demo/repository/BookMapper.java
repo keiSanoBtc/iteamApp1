d91cfe50723424b50fa25b946e4389ebbc979a88
KoharuKawakita <koharu.kawakita@bigtreetc.com>
2022-05-26 16:34:20 +0900

    mergeのためのコンフリクト解消その14(5/26/16:34)

--- itemApp1-1/src/main/java/com/example/demo/repository/BookMapper.java
+++ itemApp1-1/src/main/java/com/example/demo/repository/BookMapper.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.demo.dto.BookUserEditRequest;
+import com.example.demo.dto.UserSearchRequest;
 import com.example.demo.entity.Book;
 
 @Mapper
 public interface BookMapper {
 
-	List<Book> bookSearchedList();
+	List<Book> bookSearchedList(UserSearchRequest user);
+
+	void addPlanBook(BookUserEditRequest user);
+
+	Book lastPlanBookNum();
 
 }
